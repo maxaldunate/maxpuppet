4d44313e87d5ae2b129bafa0c4ae75f496893c96
Max Aldunate <maxaldunate@gmail.com>
2016-07-08 16:20:54 +0200

    Lecture: Adding Directory Management

--- puppetlabs/code/environments/production/modules/apache/manifests/init.pp
+++ puppetlabs/code/environments/production/modules/apache/manifests/init.pp
@@ -1,3 +1,23 @@
-class apache {
-	package { 'httpd': ensure => present, }
+class apache (
+	$package_name = $apache::params::package_name,
+	$service_name = $apache::params::service_name,
+	$vhost_dir 	  = $apache::params::vhost_dir,
+	$document_root = $apache::params::document_root,
+	$servername = $apache::params::servername,
+	$log_dir = $apache::params::log_dir
+	) inherits apache::params {
+
+	include apache::package
+	include apache::service
+
+	file { $document_root:
+		ensure 	=> directory,
+		recurse => true,
+	}
+
+	file { $log_dir:
+		ensure 	=> directory,
+		recurse => true,
+	}
+
 }
