67e819046320dc7328fb42769e96198f6875d2e1
Max Aldunate <maxaldunate@gmail.com>
2016-07-13 15:10:54 +0200

    no rula el copiado de puppetlabs/puppet

--- updateFiles.sh
+++ updateFiles.sh
@@ -3,13 +3,34 @@
 
 dir_source=''
 dir_dest=''
+
+# cpying code folder
 rm --force --recursive /etc/puppetlabs/code/
 mkdir /etc/puppetlabs/code
 cp --force --recursive --update /media/sf_maxpuppet/puppetlabs/code/* /etc/puppetlabs/code
 find /etc/puppetlabs/code -type f -print -exec chmod 644 {} \;
 find /etc/puppetlabs/code -type d -print -exec chmod 755 {} \;
 
-#rm --force /etc/puppetlabs/puppet/*
-#cp --recursive --update /media/sf_maxpuppet/puppet/* /etc/puppetlabs/puppet/
+
+# backuping ssl folder
+rm --force --recursive /root/sslbak/
+mkdir /root/sslbak
+cp --force --recursive --update /etc/puppetlabs/puppet/ssl/* /root/sslbak/
+
+
+# copying puppetlabs/puppet folder
+rm --force --recursive /etc/puppetlabs/puppet/
+mkdir /etc/puppetlabs/puppet
+cp --force --recursive --update /media/sf_maxpuppet/puppet/* /etc/puppetlabs/puppet/
+find /etc/puppetlabs/puppet/ -type f -print -exec chmod 644 {} \;
+find /etc/puppetlabs/puppet/ -type d -print -exec chmod 755 {} \;
+
+# restorying ssl folder
+mkdir /etc/puppetlabs/puppet/ssl
+cp --force --recursive --update /root/sslbak/* /etc/puppetlabs/puppet/ssl/
+find /etc/puppetlabs/puppet/ssl/ -type f -print -exec chmod 644 {} \;
+find /etc/puppetlabs/puppet/ssl/ -type d -print -exec chmod 755 {} \;
+#rm --force --recursive /root/sslbak/
+
 
 echo "Files updated!"
