cc875c9933f945a73c10a1765e01244e8f1c33fe
Max Aldunate <maxaldunate@gmail.com>
2016-07-13 18:02:19 +0200

    no print permission changes

--- updateFiles.sh
+++ updateFiles.sh
@@ -8,8 +8,8 @@ dir_dest=''
 rm --force --recursive /etc/puppetlabs/code/
 mkdir /etc/puppetlabs/code
 cp --force --recursive --update /media/sf_maxpuppet/puppetlabs/code/* /etc/puppetlabs/code
-find /etc/puppetlabs/code -type f -print -exec chmod 644 {} \;
-find /etc/puppetlabs/code -type d -print -exec chmod 755 {} \;
+find /etc/puppetlabs/code -type f -exec chmod 644 {} \;
+find /etc/puppetlabs/code -type d -exec chmod 755 {} \;
 
 
 # copying 6 files under /etc/puppetlabs/puppet/
