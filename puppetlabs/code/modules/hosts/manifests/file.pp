0e4c11b7556d6beda2ae6af5437cf1a25df7b76e
Max Aldunate <maxaldunate@gmail.com>
2016-07-14 11:28:43 +0200

    puppet resource names are CASE sensitive

--- puppetlabs/code/modules/hosts/manifests/file.pp
+++ puppetlabs/code/modules/hosts/manifests/file.pp
@@ -1,15 +1,22 @@
 class hosts::file {
+
+	#puppet resource names are CASE sensitive
 	
 	#error situation. one file twice, not possible
+	#file {'motd1':
+	#	path    => '/etc/motd',
+	#	content => 'Welcome to my server',
+	#}
 
-	file {'motd1':
-		path    => '/etc/motd',
-		content => 'Welcome to my server',
-	}
 
 	file {'motd2':
 		path    => '/etc/motd',
 		content => 'Welcome to my server 2',
+	}
+
+	#no error in this case, other resource type
+	package {'motd2':
+		name    => 'httpd',
 	}
 
 }
